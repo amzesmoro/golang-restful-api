7ec74c0b677b183dde46024e80099c565e9b5a12
amzesmoro <amzesmoro05@gmail.com>
2023-07-21 21:05:36 +0700

    binding interface DI

--- simple/injector.go
+++ simple/injector.go
@@ -30,3 +30,19 @@ func InitializedFooBarService() *FooBarService {
 	)
 	return nil
 }
+
+// Salah
+//func InitializedHelloService() *HelloService {
+//	wire.Build(NewHelloService, NewSayHelloImpl)
+//	return nil
+//}
+
+var helloSet = wire.NewSet(
+	NewSayHelloImpl,
+	wire.Bind(new(SayHello), new(*SayHelloImpl)),
+)
+
+func InitializedHelloService() *HelloService {
+	wire.Build(helloSet, NewHelloService)
+	return nil
+}
